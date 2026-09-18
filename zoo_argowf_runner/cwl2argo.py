2bffa27bd62c35613927eede916173f11f246b6f
Muhammed Nishan <nishz1072@gmail.com>
2026-09-18 17:53:51 +0530

    Fix JSON loading for workflow raw CWL

--- zoo_argowf_runner/cwl2argo.py
+++ zoo_argowf_runner/cwl2argo.py
@@ -49,7 +49,7 @@ def cwl_to_argo(
     prepare_content = f"""
 import json
 
-content = json.loads({json.dumps(json.dumps(workflow.raw_cwl))})
+content = json.loads(\"\"\"{workflow.raw_cwl}\"\"\".replace("'", '"'))
 
 inputs = "{{{{inputs.parameters.inputs}}}}"
 
