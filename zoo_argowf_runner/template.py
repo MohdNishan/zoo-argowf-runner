382c25c04e4daa1af5b4ae7f8e74ffcb988d01d7
Muhammed Nishan <nishz1072@gmail.com>
2026-09-18 17:54:34 +0530

    Clean up imports and formatting in template.py

--- zoo_argowf_runner/template.py
+++ zoo_argowf_runner/template.py
@@ -1,8 +1,6 @@
 # Description: This file contains the functions to generate the Argo workflow templates.
 from __future__ import annotations
 
-from typing import List
-
 from hera.workflows import (
     Workflow,
 )
